53f182c9f0b4d4f6a07610f078575ac6f03a39f8
Codegem <codegemorg@gmail.com>
2021-02-20 11:22:50 -0500

    Renamed the classes

--- src/Toggle/Toggle.jsx
+++ src/Toggle/Toggle.jsx
@@ -3,10 +3,10 @@ import style from "./Toggle.module.css";
 function Toggle() {
   return (
     <>
-      <input className={style.Toggle} type='checkbox' id='switch'/>
+      <input className={style.CheckBox} type='checkbox' id='switch'/>
 
-      <label className={style.Label} htmlFor='switch'>
-        <span className={style.Button}/>
+      <label className={style.Toggle} htmlFor='switch'>
+        <span className={style.Circle}/>
       </label>
     </>
   );
